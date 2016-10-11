4d37a19f5e2ea5478bdb9eeadfcc2fd0aca89d13
jinge <elvisking@whu.edu.cn>
2016-10-11 18:59:29 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/jinge/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.jinge.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jinge on 2016/9/27.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+}
