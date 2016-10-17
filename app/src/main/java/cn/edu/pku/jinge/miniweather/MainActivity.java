c921b7fe6b7b0c990206714174aa4770d9ae960c
jinge <elvisking@whu.edu.cn>
2016-10-17 22:03:40 +0800

    first commit

--- app/src/main/java/cn/edu/pku/jinge/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/jinge/miniweather/MainActivity.java
@@ -2,15 +2,63 @@ package cn.edu.pku.jinge.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by jinge on 2016/9/27.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    private ImageView mUpdateBtn;
+    private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv,
+             temperatureTv, climateTv, windTv, city_name_Tv;
+    private ImageView weatherImg, pmImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView)findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK!", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了!", Toast.LENGTH_LONG).show();
+        }
+        initView();
+    }
+
+    void initView() {
+        city_name_Tv = (TextView)findViewById(R.id.title_city_name);
+        cityTv = (TextView)findViewById(R.id.city);
+        timeTv = (TextView)findViewById(R.id.time);
+        humidityTv = (TextView)findViewById(R.id.humidity);
+        weekTv = (TextView)findViewById(R.id.week_today);
+        pmDataTv = (TextView)findViewById(R.id.pm_data);
+        pmQualityTv = (TextView)findViewById(R.id.pm2_5_quality);
+        pmImg = (ImageView) findViewById(R.id.pm2_5_img);
+        temperatureTv = (TextView)findViewById(R.id.temperature);
+        climateTv = (TextView)findViewById(R.id.climate);
+        windTv = (TextView)findViewById(R.id.wind);
+        weatherImg = (ImageView)findViewById(R.id.weather_img);
+
+        city_name_Tv.setText("N/A");
+        cityTv.setText("N/A");
+        timeTv.setText("N/A");
+        humidityTv.setText("N/A");
+        weekTv.setText("N/A");
+        pmDataTv.setText("N/A");
+        pmQualityTv.setText("N/A");
+        temperatureTv.setText("N/A");
+        climateTv.setText("N/A");
+        windTv.setText("N/A");
     }
 
 }
